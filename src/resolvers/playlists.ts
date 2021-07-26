ddfd6481df04486a20bd0f13a1d5f71d9e657587
JIdayyy <julien_abbadie@hotmail.fr>
2021-07-26 03:01:21 +0200

    playlist resolver fix

--- src/resolvers/playlists.ts
+++ src/resolvers/playlists.ts
@@ -2,14 +2,14 @@ import { Playlist, User, Song } from ".prisma/client";
 import { Queries } from "../..";
 
 export const playlistQuery: Queries<Playlist | Playlist[], { id: string }> = {
+  playlists: (parent, args, { prisma }) => {
+    return prisma.playlist.findMany();
+  },
   playlist: (parent, { id }, { prisma }) => {
     return prisma.playlist.findUnique({
       where: { id },
       rejectOnNotFound: true,
     });
-  },
-  playlists: (parent, args, { prisma }) => {
-    return prisma.playlist.findMany();
   },
 };
 
